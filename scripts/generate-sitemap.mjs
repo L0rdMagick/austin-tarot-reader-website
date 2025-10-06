c56cc6bcaf6a28c9fe0c914aef7095f40da45368
Daniel Guerrero <massivebrains@gmail.com>
2025-10-05 19:53:43 -0500

    Austin Tarot Reader Website

---
+++ scripts/generate-sitemap.mjs
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import { getSortedPostsData, getAllCategories } from '../lib/posts.js';
+
+// This is a workaround because we're running this as a script
+// We need to tell it where to find the content folder.
+process.chdir(process.cwd());
+
+const siteUrl = 'https://www.austintarotreader.com';
+
+async function generateSitemap() {
+  const posts = getSortedPostsData();
+  const categories = getAllCategories();
+
+  const staticPages = [
+    '/',
+    '/about',
+    '/services',
+    '/blog',
+    '/faq',
+    '/contact',
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${staticPages
+    .map((route) => {
+      return `
+        <url>
+          <loc>${`${siteUrl}${route}`}</loc>
+          <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+          <priority>${route === '/' ? '1.0' : '0.8'}</priority>
+        </url>
+      `;
+    })
+    .join('')}
+  ${posts
+    .map(({ slug, date }) => {
+      return `
+        <url>
+          <loc>${`${siteUrl}/blog/${slug}`}</loc>
+          <lastmod>${date}</lastmod>
+          <priority>0.9</priority>
+        </url>
+      `;
+    })
+    .join('')}
+  ${categories
+    .map(({ slug }) => {
+      return `
+        <url>
+          <loc>${`${siteUrl}/blog/category/${slug}`}</loc>
+          <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+          <priority>0.7</priority>
+        </url>
+      `;
+    })
+    .join('')}
+</urlset>
+`;
+
+  fs.writeFileSync('public/sitemap.xml', sitemap);
+  console.log('✅ Sitemap generated successfully!');
+}
+
+generateSitemap();
